635d0279745f268eac339aef01abe10525625b4a
Rupesh Kumar <rupesh.kumar@josys.com>
2024-08-03 15:16:22 +0530

    modified test

--- src/test/java/com/thoughtworks/movierental/RentalServiceIntegrationTest.java
+++ src/test/java/com/thoughtworks/movierental/RentalServiceIntegrationTest.java
@@ -15,25 +15,44 @@ class RentalServiceIntegrationTest {
     private StatementCalculator statementCalculator;
     private HtmlBuilder htmlBuilder;
 
+    private static final double EXPECTED_AMOUNT = 14.00;
+    private static final int EXPECTED_POINTS = 4;
+    private static final String EXPECTED_HTML = "<html>\n" +
+            "<head><title>Rental Statement</title></head>\n" +
+            "<body>\n" +
+            "<h1>Rental Record for John Doe</h1>\n" +
+            "<table border=\"1\">\n" +
+            "  <tr><th>Title</th><th>Amount</th></tr>\n" +
+            "  <tr>\n" +
+            "    <td>Regular Movie</td>\n" +
+            "    <td>3.50</td>\n" +
+            "  </tr>\n" +
+            "  <tr>\n" +
+            "    <td>New Release Movie</td>\n" +
+            "    <td>6.00</td>\n" +
+            "  </tr>\n" +
+            "  <tr>\n" +
+            "    <td>Children&#39;s Movie</td>\n" +
+            "    <td>4.50</td>\n" +
+            "  </tr>\n" +
+            "</table>\n" +
+            "<p>Amount owed is 14.00</p>\n" +
+            "<p>You earned 4 frequent renter points</p>\n" +
+            "</body>\n" +
+            "</html>";
+
     @BeforeEach
     void setUp() {
-        RentalAmountCalculator amountCalculator = new RentalAmountCalculator();
-        FrequentRenterPointsCalculator pointsCalculator = new FrequentRenterPointsCalculator();
-        statementCalculator = new StatementCalculator(amountCalculator, pointsCalculator);
+        statementCalculator = new StatementCalculator(
+                new RentalAmountCalculator(),
+                new FrequentRenterPointsCalculator()
+        );
         htmlBuilder = new HtmlBuilder(statementCalculator);
     }
 
     @Test
     void testCompleteRentalProcess() {
-        // Given
         Customer customer = createCustomerWithRentals();
-
-        // Expected calculations
-        double expectedAmount = 14.00;
-        int expectedPoints = 4;
-
-        // Expected HTML output with proper escaping
-        String expectedHtml = generateExpectedHtml();
 
         // When
         double actualAmount = statementCalculator.calculateTotalAmount(customer);
@@ -41,9 +60,9 @@ class RentalServiceIntegrationTest {
         String actualHtml = htmlBuilder.buildStatement(customer);
 
         // Then
-        assertEquals(expectedAmount, actualAmount, "The total amount calculated does not match the expected value.");
-        assertEquals(expectedPoints, actualPoints, "The frequent renter points calculated do not match the expected value.");
-        assertEquals(expectedHtml, actualHtml, "The generated HTML statement does not match the expected output.");
+        assertEquals(EXPECTED_AMOUNT, actualAmount, "The total amount calculated does not match the expected value.");
+        assertEquals(EXPECTED_POINTS, actualPoints, "The frequent renter points calculated do not match the expected value.");
+        assertEquals(EXPECTED_HTML, actualHtml, "The generated HTML statement does not match the expected output.");
     }
 
     private Customer createCustomerWithRentals() {
@@ -52,31 +71,5 @@ class RentalServiceIntegrationTest {
         customer.addRental(new Rental(new Movie("New Release Movie", PriceCode.NEW_RELEASE), 2));
         customer.addRental(new Rental(new Movie("Children's Movie", PriceCode.CHILDRENS), 5));
         return customer;
-    }
-
-    private String generateExpectedHtml() {
-        return "<html>\n" +
-                "<head><title>Rental Statement</title></head>\n" +
-                "<body>\n" +
-                "<h1>Rental Record for John Doe</h1>\n" +
-                "<table border=\"1\">\n" +
-                "  <tr><th>Title</th><th>Amount</th></tr>\n" +
-                "  <tr>\n" +
-                "    <td>Regular Movie</td>\n" +
-                "    <td>3.50</td>\n" +
-                "  </tr>\n" +
-                "  <tr>\n" +
-                "    <td>New Release Movie</td>\n" +
-                "    <td>6.00</td>\n" +
-                "  </tr>\n" +
-                "  <tr>\n" +
-                "    <td>Children&#39;s Movie</td>\n" +
-                "    <td>4.50</td>\n" +
-                "  </tr>\n" +
-                "</table>\n" +
-                "<p>Amount owed is 14.00</p>\n" +
-                "<p>You earned 4 frequent renter points</p>\n" +
-                "</body>\n" +
-                "</html>";
     }
 }
